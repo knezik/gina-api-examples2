c8fcb1411b32c4f019c294124f745c97692dcc93
Jan Knezik <knezik@ginasystem.com>
2015-08-07 14:15:15 +0200

    Fix: list of test modules

---
+++ gina-api-examples2/gina-api-examples2/gina_api_examples2/ViewModels/StripConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Converters;
+using Cirrious.MvvmCross.ViewModels;
+
+
+namespace gina_api_examples2.Core.ViewModels
+{
+	public class StripConverter : MvxValueConverter<string, string>
+	{
+		protected override string Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return null;
+			return value.Replace((string)parameter, string.Empty);
+		}
+	}
+}
